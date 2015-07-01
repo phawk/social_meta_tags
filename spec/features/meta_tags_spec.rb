176df4b99777c3f90107fc94a6728dd51141074d
Pete Hawkins <pete@phawk.co.uk>
2015-07-01 20:32:11 +0100

    Get the dummy app booting and under test with capybara

--- spec/features/meta_tags_spec.rb
+++ spec/features/meta_tags_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 RSpec.describe "Meta tags", type: :feature do
   scenario "The app loads" do
     visit "/"
-    expect(page).to have_content("welcome")
+    expect(page).to have_content("Welcome")
   end
 end
