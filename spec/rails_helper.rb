6bb6c90038f822332162595cb82e3c6159bbe19c
Pete Hawkins <pete@phawk.co.uk>
2015-07-01 23:27:51 +0100

    Trim down Rails dummy app for tests
    We dont need to load all of the rails framework for testing this gem. We dont need ActiveRecord, ActiveJob or ActiveModel

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,10 +24,6 @@ require 'support/meta_helper'
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
-
 RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
